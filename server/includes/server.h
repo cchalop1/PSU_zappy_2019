62ee695ba1d2583517a2b3a79363df278d27d310
cchalopin <clement.chalopin@gmail.com>
2020-06-01 18:16:04 +0200

    add: server main

---
+++ server/includes/server.h
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_zappy_2019
+** File description:
+** server
+*/
+
+#pragma once
+
+#include <stdio.h>
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+#define EXIT_ERROR 84
+#define MAX_CLIENTS 255
+#define MAX_BODY_LENGTH 512
+#define BUFFER_SIZE 5000
+// #define NB_COMMANDS 8
---
+++ server/src/main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_zappy_2019
+** File description:
+** main
+*/
+
+#include "server.h"
+
+int main(int ac, const char **av)
+{
+    printf("server start...\n");
+    return 0;
+}
