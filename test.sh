e6e9fbd009bcff0adbbe07174c5a8b321425ab27
cchalopin <clement.chalopin@gmail.com>
2020-06-04 15:06:55 +0200

    update: ci

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "start test"
-make re
-./zappy_server -x 10 -y 10 -c 2 -n name1 name2 -f 10 -p 4243 &
+make re ; ./zappy_server -x 10 -y 10 -c 2 -n name1 name2 -f 10 -p 4242 &
 cd tests
 npm i
 npm test
