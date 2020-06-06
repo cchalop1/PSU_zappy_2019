411fafd7e4161eb1bf74866dc007b4f7003abc14
aires baptiste <abaptiste@localhost.localdomain>
2020-06-06 19:40:31 +0200

    Add: first files for the ai

---
+++ client/ai_stats.py
@@ -0,0 +1,12 @@
+
+class ai_stats:
+    def __init__(self):
+        self.level = 1
+        self.food = 0
+        self.linemate = 0
+        self.deraumere = 0
+        self.sibur = 0
+        self.mendiane = 0
+        self.phiras = 0
+        self.thystame = 0
+        self.vision = []
