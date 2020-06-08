0e1fe380c7dcb9f1d0415ed946252b18193edc0c
cchalopin <clement.chalopin@gmail.com>
2020-06-09 00:04:38 +0200

    add: cmd player right left forward

---
+++ server/src/cmd_player3.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_zappy_2019
+** File description:
+** cmd_player3
+*/
+
+#include "server.h"
+
+int set(server_t* server, player_t* player, char* cmd)
+{
+    // TODO: implem
+}
+
+int incantation(server_t* server, player_t* player, char* cmd)
+{
+    // TODO: implem
+}
