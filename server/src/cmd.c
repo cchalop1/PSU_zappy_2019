98ce1b58e2a0ebab6ec7c666dbc7d5b1419101f0
Valdsj <valentin.de-saint-jacob@epitech.eu>
2020-06-08 02:41:33 +0200

    add

--- server/src/cmd.c
+++ server/src/cmd.c
@@ -14,7 +14,12 @@ int error_cmd(server_t* server, player_t* player, char* cmd)
 
 int map_size(server_t* server, player_t* player, char* cmd)
 {
+    char res[MAX_BODY_LENGTH];
+    int size = server->map.x_max * server->map.y_max;
+
+    sprintf(res, "%d", size);
     printf("CMD map size\n");
+    send_reply(player->fd, res);
 }
 
 int content_of_a_tile(server_t* server, player_t* player, char* cmd)
@@ -29,5 +34,12 @@ int content_of_all_tile(server_t* server, player_t* player, char* cmd)
 
 int name_of_all_teams(server_t* server, player_t* player, char* cmd)
 {
+    char res[MAX_BODY_LENGTH] = {0};
+
     printf("CMD name_of_all_teams\n");
+    for (int x = 0; server->team_names[x]; x++) {
+        strcat(res, server->team_names[x]);
+        strcat(res, "\n");
+    }
+    send_reply(player->fd, res);
 }
