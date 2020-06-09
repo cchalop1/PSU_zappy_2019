17751199623b57bd079a17012bd9d723c1f077fc
cchalopin <clement.chalopin@gmail.com>
2020-06-09 19:28:41 +0200

    add: Inventory commande

--- server/src/handle_cmd.c
+++ server/src/handle_cmd.c
@@ -71,6 +71,12 @@ static void new_player(server_t* server, player_t* player, int i)
 
     player->type = PLAYER;
     player->team_name = strdup(server->team_names[i]);
+    player->inventory[0] = 0;
+    player->inventory[1] = 0;
+    player->inventory[2] = 0;
+    player->inventory[3] = 0;
+    player->inventory[4] = 0;
+    player->inventory[5] = 0;
     map_size(server, player, "");
     if (graphic_player != NULL)
         player_info(server, player, graphic_player->fd);
