05180abbb025bb7479fe78e75bfe595a557d47ab
aires baptiste <abaptiste@localhost.localdomain>
2020-06-09 19:02:34 +0200

    WIP: Add : automatised list of actions , usefull functions

--- client/ai.py
+++ client/ai.py
@@ -1,5 +1,6 @@
-from ai_actions import ai_actions, act_dur
+from ai_actions import ai_actions, act_dur, recipe
 from ai_stats import ai_stats
+from path import path
 
 class ai(ai_actions, ai_stats):
     def __init__(self):
@@ -7,39 +8,51 @@ class ai(ai_actions, ai_stats):
         pass
 
     def __del__(self):
-        print("I'm starving ...")
-
-    def __call__(self):
         pass
 
-    def TryAction(self, func):
-        if self.food * 126 >= act_dur[func]:
+    def loop(self):
+        while 1:
+            aswer = self.get_return()
+            self.select_action()
+
+    def select_action(self):
+        pass
+
+    def have_time(self, func):
+        if self.time_unit >= act_dur[func]:
             return True
         return False
 
-    def GetReturn(self):
+    def get_return(self):
         ret = ""
         return ret
 
-    def GetInventory(self):
+    def is_dead(self):
+        print("I'm starving ...")
+        self.__del__()
+
+    def get_connect_nbr(self):
+        self.Connect_nbr()
+        return self.get_return()
+
+    def get_inventory(self):
         self.Inventory()
-        ret = self.GetReturn()
+        ret = self.get_return()
         ret.replace(']', '')
         ret.replace(',', '')
         ret.split(' ')
 
-        self.food = int(ret[1])
-        self.linemate = int(ret[3])
-        self.deraumere = int(ret[5])
-        self.sibur = int(ret[7])
-        self.mendiane = int(ret[9])
-        self.phiras = int(ret[11])
-        self.thystame = int(ret[13])
+        self.inventory["linemate"] = int(ret[1])
+        self.inventory["deraumere"] = int(ret[3])
+        self.inventory["sibur"] = int(ret[5])
+        self.inventory["mendiane"] = int(ret[7])
+        self.inventory["phiras"] = int(ret[9])
+        self.inventory["thystame"] = int(ret[11])
 
-    def GetLook(self):
+    def get_look(self):
         self.vision.clear()
         self.Look()
-        ret = self.GetReturn()
+        ret = self.get_return()
         ret.replace(']', '')
         ret.replace('[', '')
         ret.split(', ')
@@ -47,3 +60,64 @@ class ai(ai_actions, ai_stats):
             a = i.split(' ')
             self.vision.append(a)
 
+    def can_lvl_up(self):
+        if (self.inventory["linemate"] < recipe[self.level]["linemate"] or
+        self.inventory["deraumere"] < recipe[self.level]["deraumere"] or
+        self.inventory["sibur"] < recipe[self.level]["sibur"] or
+        self.inventory["mendiane"] < recipe[self.level]["mendiane"] or
+        self.inventory["phiras"] < recipe[self.level]["phiras"] or
+        self.inventory["thystame"] < recipe[self.level]["thystame"]):
+            return False
+        return True
+
+    def needed(self):
+        if self.inventory["thystame"] < recipe[self.level]["thystame"]:
+            return "thystame"
+        elif self.inventory["phiras"] < recipe[self.level]["phiras"]:
+            return "phiras"
+        elif self.inventory["mendiane"] < recipe[self.level]["mendiane"]:
+            return "mendiane"
+        elif self.inventory["sibur"] < recipe[self.level]["sibur"]:
+            return "sibur"
+        elif self.inventory["deraumere"] < recipe[self.level]["deraumere"]:
+            return "deraumere"
+        elif self.inventory["linemate"] < recipe[self.level]["linemate"]:
+            return "linemate"
+        else:
+            return ""
+
+    def go_to(self, path):
+        for i in range(0, path.forward):
+            self.Forward()
+        if path.left > 0:
+            self.Left()
+            for i in range(0, path.left):
+                self.Forward()
+        elif path.right() > 0:
+            self.Right()
+            for i in range(0, path.left):
+                self.Forward()
+
+    def go_to_n(self, n):
+        p = path(0,0,0)
+        return p
+
+    def _find_that(self, obj):
+        i = 0
+        while (i < len(self.vision)):
+            if obj in self.vision[i]:
+                pass ####
+            i += 1
+        return 0 ###
+
+    def find_that(self, obj):
+        p = path(0,0,0)
+        self.get_look()
+        n = self._find_that(obj)
+        return p
+
+    def get_object(self, obj):
+        p = self.find_that(obj)
+        self.go_to(p)
+        self.Take(obj)
+        pass
