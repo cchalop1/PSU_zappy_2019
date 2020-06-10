c44074aa940605236afe81e2ed48c66bf9f4af68
cchalopin <clement.chalopin@gmail.com>
2020-06-10 15:40:17 +0200

    add: eject cmd for player

--- server/src/cmd_player2.c
+++ server/src/cmd_player2.c
@@ -24,11 +24,25 @@ int fork_cmd_player(server_t* server, player_t* player, char* cmd)
 
 int eject(server_t* server, player_t* player, char* cmd)
 {
-    // TODO: implem
+    player_t* player_copy = server->players;
+    bool player_is_eject = false;
+
+    for (;player_copy; player_copy = player_copy->next) {
+        if (player->pos_x == player_copy->pos_x
+            && player->pos_y == player_copy->pos_y) {
+                forward(server, player_copy, "");
+                send_reply(player->fd, "ok\n");
+                player_is_eject = true;
+            }
+    }
+    if (!player_is_eject) {
+        send_reply(player->fd, "ko\n");
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
 
 int take(server_t* server, player_t* player, char* cmd)
 {
     // TODO: implem
 }
-
