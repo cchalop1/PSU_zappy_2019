20214e9f42f7bb3af53af0b580b05472f4d8d346
Valdsj <valentin.de-saint-jacob@epitech.eu>
2020-06-10 19:24:51 +0200

    MOD: finished look

--- server/src/look.c
+++ server/src/look.c
@@ -30,19 +30,31 @@ static tile_t *get_tile(server_t *s, player_t *p, int x, int y)
     }
 }
 
+static int check_player(server_t *s, tile_t *t)
+{
+    player_t *copy = s->players;
+    int total = 0;
+
+    for (; copy; copy = copy->next)
+        if (copy->pos_x == t->x && copy->pos_y == t->y)
+            total++;
+    return total;
+}
+
 int vision(server_t *s, player_t *p)
 {
     char res[MAX_BODY_LENGTH];
     char *temp = res;
     tile_t *tile = NULL;
 
-    sprintf(temp, "[");
-    for (int x = 0; x < p->level; x++)
+    temp += sprintf(temp, "[");
+    for (int x = 0; x <= 2 + p->level; x++)
         for (int y = 0; y < 2 * x + 1; y++) {
             tile = get_tile(s, p, x, y - x);
-            sprintf(temp, "%s, ", content_for_one_tile(s, tile->x, tile->y));
+            temp += sprintf(temp, "%d %s, ", check_player(s, tile), \
+            content_for_one_tile(s, tile->x, tile->y));
         }
-    sprintf(temp, "]\n");
+    temp += sprintf(temp, "]\n");
     send_reply(p->fd, res);
     return EXIT_SUCCESS;
 }
