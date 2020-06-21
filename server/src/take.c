60c40898af0448fd86d890710b83034c19f94cf9
cchalopin <clement.chalopin@gmail.com>
2020-06-21 19:14:15 +0200

    add: populate map when player take

--- server/src/take.c
+++ server/src/take.c
@@ -7,12 +7,27 @@
 
 #include "server.h"
 
+static void populate_map(server_t* server, bool is_fruit)
+{
+    int x = rand() % server->map.x_max;
+    int y = rand() % server->map.y_max;
+
+    if (is_fruit && server->map.tiles[y][x].fruit)
+        populate_map(server, is_fruit);
+    else if (is_fruit)
+        server->map.tiles[y][x].fruit = true;
+    else
+        server->map.tiles[y][x].stones[rand() % 6] += 1;
+    return;
+}
+
 static int player_eat_food(server_t* server, player_t* player)
 {
     if (server->map.tiles[player->pos_y][player->pos_x].fruit) {
         player->life += (126.0 / server->freq) * 1000.0;
         send_reply(player->fd, "ok\n");
         server->map.tiles[player->pos_y][player->pos_x].fruit = false;
+        populate_map(server, true);
     } else {
         send_reply(player->fd, "ko\n");
         return EXIT_FAILURE;
@@ -36,6 +51,7 @@ int take(server_t* server, player_t* player, char* cmd)
                 server->map.tiles[player->pos_y][player->pos_x].stones[i]--;
                 player->inventory[i]++;
                 send_reply(player->fd, "ok\n");
+                populate_map(server, true);
                 break;
             } else
                 send_reply(player->fd, "ko\n");
